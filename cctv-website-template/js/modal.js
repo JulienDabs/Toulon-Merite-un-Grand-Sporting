498c7e2ab05158e326ea16578c2b6e8f737c3b17
unknown <j.dabadie81@gmail.com>
2023-04-05 22:15:52 +0200

    final exemple

--- cctv-website-template/js/modal.js
+++ cctv-website-template/js/modal.js
@@ -1,25 +1,22 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var modalBtns = [...document.querySelectorAll(".modal-toggle")];
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+var closeBtns = [...document.querySelectorAll(".modal-close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
     modal.style.display = "none";
+  }
+});
+
+window.onclick = function(event) {
+  console.log(event);
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
   }
 }
